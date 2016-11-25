40c868718e1db92d48f931ca9d983182de6d9f43
Ian <ian.orellana@usach.cl>
2016-11-25 16:38:44 -0300

    Se agrega EDA para RNA

---
+++ src/EDA/RNAStructure.java
@@ -0,0 +1,66 @@
+package EDA;
+
+public class RNAStructure {
+	private String miRNA_id;
+	private String miRNA;
+	private String lncRNA_id;
+	private String lncRNA;
+	private int position;
+	
+	public RNAStructure(){
+		this.miRNA_id=null;
+		this.setMiRNA(null);
+		this.setLncRNA_id(null);
+		this.setLncRNA(null);
+		this.setPosition(0);
+	}
+	
+	public RNAStructure(String miRNA_id,String miRNA,String lncRNA_id,String lncRNA,int position){
+		this.miRNA_id=miRNA_id;
+		this.setMiRNA(miRNA);
+		this.setLncRNA_id(lncRNA);
+		this.setLncRNA(lncRNA);
+		this.setPosition(position);
+	}
+	
+	public String getmiRNA_id(){
+		return miRNA_id;
+	}
+	
+	public void setmiRNA_id(String miRNA_id){
+		this.miRNA_id=miRNA_id;
+	}
+
+	public String getMiRNA() {
+		return miRNA;
+	}
+
+	public void setMiRNA(String miRNA) {
+		this.miRNA = miRNA;
+	}
+
+	public String getLncRNA_id() {
+		return lncRNA_id;
+	}
+
+	public void setLncRNA_id(String lncRNA_id) {
+		this.lncRNA_id = lncRNA_id;
+	}
+
+	public String getLncRNA() {
+		return lncRNA;
+	}
+
+	public void setLncRNA(String lncRNA) {
+		this.lncRNA = lncRNA;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	
+}
